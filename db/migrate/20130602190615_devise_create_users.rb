2df46d4dd176116e24f7f639e64c472e7eac4ba7
Tomasz Foster <tomaszfoster@gmail.com>
2013-06-05 20:53:19 -0400

    remove duplicate email reference in db

--- db/migrate/20130602190615_devise_create_users.rb
+++ db/migrate/20130602190615_devise_create_users.rb
@@ -6,7 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :profile_name
 
       ## Database authenticatable
-      #t.string :email,              :null => false, :default => ""
+      t.string :email,              :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
 
       ## Recoverable
@@ -41,7 +41,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps
     end
 
-    add_index :users, :email,                :unique => true
+    #add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
 
   end
