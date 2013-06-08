350a1aea5d7cf49b931c7fc4f82e5675644b8675
Tomasz Foster <tomaszfoster@gmail.com>
2013-06-08 15:02:04 -0400

    Updated authentication system

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :first_name, :last_name, :profile_name, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
+                  :first_name, :last_name, :profile_name
   # attr_accessible :title, :body
 
   has_many :statuses
